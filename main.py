d4ef0109f7323b464ae4bf04df449ce1b0e01391
Atharva Satam <atharvasatam9922@gmail.com>
2026-09-23 22:01:30 +0200

    Initial project

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import joblib
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+model = joblib.load("model.pkl")
+
+
+@app.post("/predict")
+def predict(study_hours: float):
+
+    prediction = model.predict([[study_hours]])
+
+    return {
+        "study_hours": study_hours,
+        "predicted_marks": prediction[0]
+    } 
